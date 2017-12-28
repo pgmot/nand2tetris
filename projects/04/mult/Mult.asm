2ba69bdf3ada7e0d6d5ddb5ddd42cff462f15527
Daisuke Sakamoto <daisuke.sakamoto@mixi.co.jp>
2017-12-28 21:20:16 +0900

    complete 04 (w/o fill)

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -7,3 +7,31 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+  @2
+  M=0
+  @1
+  D=M
+  @count
+  M=D
+
+(LOOP)
+  @count
+  D=M
+  @END
+  D; JEQ
+
+  @2
+  D=M
+  @0
+  D=D+M
+  @2
+  M=D
+  @count
+  M=M-1
+  @LOOP
+  0; JMP
+
+(END)
+
+  @END
+  0; JMP
